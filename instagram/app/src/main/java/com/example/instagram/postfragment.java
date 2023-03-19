facd6320cec540598abb9f1568e335721402bf0b
neyut <tuyen231102b@gmail.com>
2023-03-19 23:23:12 +0700

    commit view

---
+++ instagram/app/src/main/java/com/example/instagram/postfragment.java
@@ -0,0 +1,14 @@
+package com.example.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class postfragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_postfragment);
+    }
+}
